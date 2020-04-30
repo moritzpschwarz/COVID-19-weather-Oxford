7b645e51f273ea3979556ca4a159746e274ff56a
moritzpschwarz <moritz.schwarz@outlook.com>
2020-04-30 23:34:48 +0200

    Update 1.1 Round Location Data.R

--- Code/1. Data Preparation/1.1 Round Location Data.R
+++ Code/1. Data Preparation/1.1 Round Location Data.R
@@ -1,8 +1,8 @@
 library(tidyverse)
 
-df <- readxl::read_excel("C:/Users/morit/OneDrive - Nexus365/Covid-19 Paper/Data/Locations_29032020.xlsx",col_names = c("province", "latitude","longitude","location_ID","country"))
+df <- readxl::read_excel("C:/Users/morit/OneDrive - Nexus365/Covid-19 Paper/Data/Locations_30042020.xlsx",col_names = c("province", "latitude","longitude","location_ID","country"))
 
 df %>% 
   mutate(longitude = round(longitude,2),
          latitude = round(latitude,2)) %>% 
-  write.csv(.,"C:/Users/morit/OneDrive - Nexus365/Covid-19 Paper/Data/Locations_29032020_rounded.csv",row.names=F)
+  write.csv(.,"C:/Users/morit/OneDrive - Nexus365/Covid-19 Paper/Data/Locations_30042020_rounded.csv",row.names=F)
