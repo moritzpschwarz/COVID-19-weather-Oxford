dd43818b8395a40a5a458ca6af98dc8090a2ea83
moritzpschwarz <moritz.schwarz@outlook.com>
2020-05-11 20:30:14 +0200

    cleaned repo

--- Code/1. Data Preparation/1.1 Round Location Data.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-df <- readxl::read_excel("C:/Users/morit/OneDrive - Nexus365/Covid-19 Paper/Data/Locations_30042020.xlsx",col_names = c("province", "latitude","longitude","location_ID","country"))
-
-df %>% 
-  mutate(longitude = round(longitude,2),
-         latitude = round(latitude,2)) %>% 
-  write.csv(.,"C:/Users/morit/OneDrive - Nexus365/Covid-19 Paper/Data/Locations_30042020_rounded.csv",row.names=F)
